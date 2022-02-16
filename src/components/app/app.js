33cabd93b6d60c2e364005911a0ef62339776c3a
Artyom <art161994@yandex.ru>
2022-02-16 17:31:15 +0300

    modified employyes list add check for form and add counters in app-info

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,10 +12,10 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      data: [{name: 'John C.', salary: 1000, increase: false, id: 1},
-        {name: 'Juan G.', salary: 900, increase: true, id: 2},
-        {name: 'Ury L.', salary: 1500, increase: false, id: 3},
-        {name: 'Max F.', salary: 2200, increase: false, id: 4}
+      data: [{name: 'John C.', salary: 1000, increase: false, rise: true, id: 1},
+        {name: 'Juan G.', salary: 900, increase: false, rise: false, id: 2},
+        {name: 'Ury L.', salary: 1500, increase: true, rise: false, id: 3},
+        {name: 'Max F.', salary: 2200, increase: false, rise: false, id: 4}
       ]
     }
 
@@ -36,6 +36,7 @@ class App extends Component{
       name,
       salary,
       increase: false,
+      rise: false,
       id: this.maxId++
     }
     this.setState(({data}) => {
@@ -46,10 +47,28 @@ class App extends Component{
     });
   }
 
+  onToggleProp = (id, prop) => {
+    this.setState(({data}) => ({
+      data: data.map(item => {
+        if(item.id === id) {
+          return {...item, [prop]: !item[prop]}
+        }
+        return item;
+      })
+    }))
+  }
+
+
   render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length;
+    const rised = this.state.data.filter(item => item.rise).length;
     return (
     <div className='app'>
-      <AppInfo />
+      <AppInfo 
+        employees={employees} 
+        increased={increased}
+        rised={rised}/>
 
       <div className='search-panel'>
           <SearchPanel/>
@@ -58,7 +77,8 @@ class App extends Component{
         
       <EmployeesList 
         data={this.state.data}
-        onDelete={this.deleteItem}/>
+        onDelete={this.deleteItem}
+        onToggleProp={this.onToggleProp}/>
       <EmployeesAddForm onAdd={this.addItem}/>    
     </div>
   );
